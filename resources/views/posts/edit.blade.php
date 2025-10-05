db059600dc572dc599a0d97de58ecfcad8565676
santuele-cl <santuele.cl@gmail.com>
2025-10-05 20:11:27 +0800

    feat: daisyui

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,9 +1,11 @@
-<form method="POST" action="{{ route('posts.update', $post->id) }}">
-    @csrf
-    @method('PUT')
-    <input type="text" name="title" placeholder="Title" value="{{ $post->title }}">
-    <textarea name="content" placeholder="Content">{{ $post->content }}</textarea>
-    <button type="submit">Submit</button>
-</form>
+<x-layout>
+    <form method="POST" action="{{ route('posts.update', $post->id) }}">
+        @csrf
+        @method('PUT')
+        <input class="input" type="text" name="title" placeholder="Title" value="{{ $post->title }}">
+        <textarea class="textarea" name="content" placeholder="Content">{{ $post->content }}</textarea>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</x-layout>
 
 
